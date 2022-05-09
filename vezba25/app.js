d98b0dda961450742bdaf5e1c3b6158d635256b1
daris <daris.hodzic.dh@gmail.com>
2022-05-09 20:40:41 +0200

    js

---
+++ vezba25/app.js
@@ -0,0 +1,36 @@
+//Primitive data types:
+
+//string:
+// "a234821390sadjlsakd"
+//number:
+// 25
+//boolean:
+// true , false
+
+
+// +
+// -
+// *
+// /
+// **
+// %
+// https://www.w3schools.com/js/js_arithmetic.asp
+
+
+
+// console.log(true && true)
+// console.log(true && false)
+// console.log(false&&true)
+// console.log(false && false)
+
+
+// console.log(true || true)
+// console.log(true || false)
+// console.log(false || true)
+// console.log(false || false)
+
+
+//https://www.w3schools.com/js/js_operators.asp
+
+//https://www.youtube.com/watch?v=FSs_JYwnAdI&list=PL0vfts4VzfNixzfaQWwDUg3W5TRbE7CyI&index=2
+//https://www.youtube.com/watch?v=DHjqpvDnNGE
