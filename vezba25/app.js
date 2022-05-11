8d563c78dfa8da8b8135a83531fe4d1e0260739e
daris <daris.hodzic.dh@gmail.com>
2022-05-11 20:59:15 +0200

    commit

--- vezba25/app.js
+++ vezba25/app.js
@@ -7,7 +7,6 @@
 //boolean:
 // true , false
 
-
 // +
 // -
 // *
@@ -16,21 +15,64 @@
 // %
 // https://www.w3schools.com/js/js_arithmetic.asp
 
-
-
 // console.log(true && true)
 // console.log(true && false)
 // console.log(false&&true)
 // console.log(false && false)
-
 
 // console.log(true || true)
 // console.log(true || false)
 // console.log(false || true)
 // console.log(false || false)
 
-
 //https://www.w3schools.com/js/js_operators.asp
 
 //https://www.youtube.com/watch?v=FSs_JYwnAdI&list=PL0vfts4VzfNixzfaQWwDUg3W5TRbE7CyI&index=2
 //https://www.youtube.com/watch?v=DHjqpvDnNGE
+
+// unos1 = prompt("Enter your num1: ");
+// unos2 = prompt("Enter your num2: ");
+
+// if (unos1 > unos2) {
+//   console.log("Welcome!");
+// }
+
+// unos = prompt("Enter your num1: ");
+
+// if (unos % 2 === 0) {
+//   console.log("broj je paran!");
+// } else {
+//   console.log("broj je neparan!");
+// }
+
+// console.log(Boolean(0));
+// console.log(Boolean(undefined));
+// console.log(Boolean(false));
+// console.log(Boolean(""));
+// console.log(Boolean(null));
+
+// unos = prompt("Enter your num1: ");
+
+// if (unos >= 0) {
+//   console.log("broj je pozitivan!");
+// } else {
+//   console.log("broj je negativan!");
+// }
+
+unos = prompt("Enter your num1: ");
+
+if (
+  (unos % 100 === 0 && unos % 400 === 0) ||
+  (unos % 100 !== 0 && unos % 4 === 0)
+) {
+  console.log("broj prestupan!");
+} else {
+  console.log("broj nije prestupan!");
+}
+
+//DOmaci uneeti broj bodova ucenika i ispisati ocenu ucenika.
+//0-20 bodova - 1
+//21-40 bodova - 2
+//41-60 bodova - 3
+//61-80 bodova - 4
+//81-100 bodova - 5
